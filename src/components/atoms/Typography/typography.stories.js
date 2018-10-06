c701bb64b53afa215f8255436e4c37f028d9f1d8
Martin Kogut <mail@martinkogut.de>
2018-10-06 22:55:34 +0200

    Added more typography examples

--- src/components/atoms/Typography/typography.stories.js
+++ src/components/atoms/Typography/typography.stories.js
@@ -7,9 +7,12 @@ import { linkTo } from '@storybook/addon-links'
 import '../../../assets/scss/base.scss'
 
 storiesOf('Atoms/Typography', module)
-.add('h1', () => <h1>THis is wildstyle</h1>)
-.add('h2', () => <h2>THis is wildstyle</h2>)
-.add('h3', () => <h3>THis is wildstyle</h3>)
-.add('h4', () => <h4>THis is wildstyle</h4>)
-.add('h5', () => <h5>THis is wildstyle</h5>)
-.add('h6', () => <h6>THis is wildstyle</h6>)
+.add('h1', () => <h1>This is wildstyle</h1>)
+.add('h2', () => <h2>This is wildstyle</h2>)
+.add('h3', () => <h3>This is wildstyle</h3>)
+.add('h4', () => <h4>This is wildstyle</h4>)
+.add('h5', () => <h5>This is wildstyle</h5>)
+.add('h6', () => <h6>This is wildstyle</h6>)
+.add('Dropcaps', () => <p className="dropcaps">This is wildstyle</p>)
+.add('Heading alternative', () => <h1 className="h-alt">This is wildstyle</h1>)
+.add('Link', () => <p><a>This is wildstyle</a></p>)

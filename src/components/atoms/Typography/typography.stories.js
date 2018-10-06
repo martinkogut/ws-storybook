85798a8b200e246fa210e01aec6af97bc3191864
Martin Kogut <mail@martinkogut.de>
2018-10-06 22:48:06 +0200

    added typography from h1 - h6

--- src/components/atoms/Typography/typography.stories.js
+++ src/components/atoms/Typography/typography.stories.js
@@ -7,4 +7,9 @@ import { linkTo } from '@storybook/addon-links'
 import '../../../assets/scss/base.scss'
 
 storiesOf('Atoms/Typography', module)
-.add('h1', () => <h2>What we do</h2>)
+.add('h1', () => <h1>THis is wildstyle</h1>)
+.add('h2', () => <h2>THis is wildstyle</h2>)
+.add('h3', () => <h3>THis is wildstyle</h3>)
+.add('h4', () => <h4>THis is wildstyle</h4>)
+.add('h5', () => <h5>THis is wildstyle</h5>)
+.add('h6', () => <h6>THis is wildstyle</h6>)

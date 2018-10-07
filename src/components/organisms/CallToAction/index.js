ee486daf7da739e050c10530c379a4cec4fe838d
Martin Kogut <mail@martinkogut.de>
2018-10-07 23:37:19 +0200

    added cta

--- src/components/organisms/CallToAction/index.js
+++ src/components/organisms/CallToAction/index.js
@@ -8,17 +8,16 @@ import Typography from '../../atoms/Typography'
 
 class CTA extends React.Component {
   render () {
-    console.log(styles)
-    let ctaClasses = classnames({
-      'cta-link': true,
-      'cta-dark': true
+    let ctaStyles = classnames({
+      [styles['cta-link']]: true,
+      [styles['cta-dark']]: this.props.dark
     })
     return (
-      <div className={styles['cta-link', 'cta-dark']}>
+      <div className={ctaStyles}>
         <div className={styles['bg-overlay']}>
           <div className={styles['cta-wrapper']}>
-            <Typography heading="h3 h-alt">See what we can do for you</Typography>
-            <Button style="btn-light btn-large">Get in touch</Button>
+          <h3 class="h-alt wow fadeIn">See what we can do for you</h3>
+            <Button light large>Get in touch</Button>
             {/*
             <h3 class="h-alt wow fadeIn" data-wow-delay=".1s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.1s; animation-name: fadeIn;">See what we can do for you</h3>
             <a href="#contact" class="btn btn-light btn-large wow fadeIn" data-wow-delay=".3s" data-wow-duration="1s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeIn;">Get in touch</a>

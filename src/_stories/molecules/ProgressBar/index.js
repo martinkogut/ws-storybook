60eb55f3a0af058bb69aec83c1ea65cdc9da4e2e
Martin Kogut <mail@martinkogut.de>
2018-10-24 11:56:38 +0200

    added thin progressbar

--- src/_stories/molecules/ProgressBar/index.js
+++ src/_stories/molecules/ProgressBar/index.js
@@ -5,10 +5,16 @@ import { optionalSelect } from '../../../components/utils/optionalSelect';
 import readme from './README.md';
 import ProgressBar from '../../../components/molecules/ProgressBar';
 
+const options = {
+  'Default': null,
+  'Thin': 'thin'
+};
+
 const component = () => (
   <ProgressBar
     text={text('Text', 'Web Design')}
     percent={text('Percentage', '80')}
+    context={optionalSelect('Context', options, '')}
   />
 );
 
--- src/components/molecules/ProgressBar/index.js
+++ src/components/molecules/ProgressBar/index.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import cx from 'classnames'
 
-const ProgressBar = ({ percent, text }) => {
+const ProgressBar = ({ percent, text, context, className }) => {
 
   const baseClass = 'progress-group'
-  const rootClass = cx(baseClass)
+
+  const subClass = cx(baseClass, className, {
+    [`progress-${context}`]: context
+  })
 
   return (
-    <div class={rootClass}>
-      <div className="progress">
+    <div className={baseClass}>
+      <div className={`progress ${context ? 'progress-thin' : null}`}>
         <div className="progress-bar" role="progressbar" aria-valuenow={percent} aria-valuemin="0" aria-valuemax="100" style={{width: percent+'%'}}>
           <span className="sr-only">{percent}% Complete</span>
         </div>
@@ -22,7 +25,8 @@ const ProgressBar = ({ percent, text }) => {
 
 ProgressBar.propTypes = {
   percent: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired
+  text: PropTypes.string.isRequired,
+  context: PropTypes.string
 }
 
 export default ProgressBar

2104067a8d6c83f3fe34c61336b3b295e88cd36c
Martin Kogut <mail@martinkogut.de>
2018-11-02 12:13:57 +0100

    added pages and templates

---
+++ src/_stories/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withReadme } from 'storybook-readme';
+import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
+
+const stories = storiesOf('Pages', module);
+const storyWrapper = story => {
+    return <div style={{ margin: '35px' }}>{story()}</div>;
+};
+
+stories
+    .addDecorator((story, context) => withInfo('')(story)(context))
+    .addDecorator(storyWrapper)
+    .addDecorator(withKnobs)

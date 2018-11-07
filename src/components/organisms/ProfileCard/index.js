ad30c6d22dd2e9561919efd2b8836ff13a72856a
Martin Kogut <mail@martinkogut.de>
2018-11-07 22:25:21 +0100

    fixedn profile card

--- src/components/organisms/ProfileCard/index.js
+++ src/components/organisms/ProfileCard/index.js
@@ -11,22 +11,28 @@ const ProfileCard = ({ name, position, className, style }) => {
   const rootClass = cx(baseClass)
 
   return (
-    <div className={rootClass}>
-      <div className="t-item">
-        <div className="t-image">
-          <Image src="http://themes.89elements.com/definity-html/assets/images/team-1.jpg" alt="Team Member" />
-        </div>
-        <div className="t-info">
-          <h4 className="t-name">{name}</h4>
-          <span className="t-role">{position}</span>
-          <ul className="social-links">
-            <li><a href="#"><i className="fa fa-facebook"></i></a></li>
-            <li><a href="#"><i className="fa fa-twitter"></i></a></li>
-            <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-            <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
-          </ul>
+    <div className="container team-3col">
+      <div className="row section">
+        <div className="col-md-4 col-xs-6 mb-sm-50">
+          <div className={rootClass}>
+            <div className="t-item">
+              <div className="t-image">
+                <Image src="http://themes.89elements.com/definity-html/assets/images/team-1.jpg" alt="Team Member" />
+              </div>
+              <div className="t-info">
+                <h4 className="t-name">{name}</h4>
+                <span className="t-role">{position}</span>
+                <ul className="social-links">
+                  <li><a href="#"><i className="fa fa-facebook"></i></a></li>
+                  <li><a href="#"><i className="fa fa-twitter"></i></a></li>
+                  <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
+                  <li><a href="#"><i className="fa fa-dribbble"></i></a></li>
+                </ul>
+              </div>
+            </div>
         </div>
       </div>
+    </div>
   </div>
   );
 }
